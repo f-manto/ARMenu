706473fa46c00064a4b19cc7ed0c53c7b3792c50
f-manto <fra.mantovani@gmail.com>
2017-11-19 17:17:47 -0600

    project created

--- ARMenu/ViewController.swift
+++ ARMenu/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
 
+    @IBOutlet weak var _email: UITextField!
+    @IBOutlet weak var _password: UITextField!
+    
+    @IBOutlet weak var _login_button: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let preferences = UserDefaults.standard
+        
+        if(preferences.object(forKey: "session") != nil)
+        {
+            LoginDone()
+        }
+        else
+        {
+            LoginToDo()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Login_Button(_ sender: Any) {
+    }
+    
+    func LoginToDo()
+    {
+        _email.isEnabled = true
+        _password.isEnabled = true
+        
+        _login_button.setTitle("Login", for: .normal)
+    }
+    
+    func LoginDone()
+    {
+        _email.isEnabled = false
+        _password.isEnabled = false
+        
+        _login_button.setTitle("Logout", for: .normal)
+    }
+    
+    @IBAction func didTouchRegister(_ sender: Any) {
+        performSegue(withIdentifier: "FromLoginToRegister", sender: self)
+    }
 }
 
